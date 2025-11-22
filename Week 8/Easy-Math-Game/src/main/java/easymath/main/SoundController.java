94d7a36e04534c85a273c038b209a553d39ec763
BLAXK <nutsukiamsr3@gmail.com>
2025-11-22 21:20:45 +0700

    Add files📝

---
+++ Week 8/Easy-Math-Game/src/main/java/easymath/main/SoundController.java
@@ -0,0 +1,59 @@
+package easymath.main;
+
+/**
+ *
+ * @author BLAXK
+ */
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import javazoom.jl.player.Player;
+
+public class SoundController {
+    private EasyMathGame game;
+    private Player player;
+    private String fileName;
+    
+    public SoundController(EasyMathGame game) {
+        this.game = game;
+    }
+    
+    public void close() {
+        if (player != null) {
+            player.close();
+        }
+    }
+    
+    private Player getSoundFile(int selectedSound) {
+        switch(selectedSound) {
+            case 0 :
+                fileName = "Correct.mp3";
+                break;
+            case 1 :
+                fileName = "Wrong.mp3";
+                break;
+            default :
+                fileName = "EndGame.mp3";
+        }
+        try {
+            InputStream inputStream = getClass().getResourceAsStream("/sounds/" + fileName);
+            BufferedInputStream buffer = new BufferedInputStream(inputStream);
+            return new Player(buffer);
+        } catch (Exception e) {
+            game.callOptionPane(e.toString(), "Error", 0);
+            return null;
+        }
+    }
+        
+    public void play(int selectSound) {
+        close();
+        new Thread(() -> {
+                try {
+                    getSoundFile(selectSound).play();
+                } 
+                catch (Exception e) {
+                    game.callOptionPane(e.toString(), "Error", 0);
+                }
+        }).start();
+    }
+}
